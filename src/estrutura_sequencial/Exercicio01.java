c00937a1f4fa16017117b6245d67baeb0e796f8e
Elizangela Xavier <elizangelaxavier913@gmail.com>
2023-05-29 20:49:39 -0300

    xercício 02

--- src/estrutura_sequencial/Exercicio01.java
+++ src/estrutura_sequencial/Exercicio01.java
@@ -3,7 +3,8 @@ package estrutura_sequencial;
 import java.util.Scanner;
 
 /*
-
+Faça um programa para ler dois valores inteiros, e depois mostrar na tela a soma desses números com uma
+mensagem explicativa.
  */
 public class Exercicio01 {
     public static void main(String[] args) {
---
+++ src/estrutura_sequencial/Exercicio02.java
@@ -0,0 +1,25 @@
+package estrutura_sequencial;
+
+
+import java.util.Locale;
+import java.util.Scanner;
+/*Faça um programa para ler o valor do raio de um círculo, e depois mostrar o valor da área deste círculo com quatro
+     casas decimais.
+        Fórmula da área: area = π . raio2
+        Considere o valor de π = 3.14159*/
+
+public class Exercicio02 {
+
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner leia = new Scanner(System.in);
+
+
+        System.out.println("Digite o valor do raio de um circulo:");
+        double raio = leia.nextDouble();
+
+        double area = 3.14159 * Math.pow(raio, 2);
+
+        System.out.printf("A = %.4f ", area);
+    }
+}
