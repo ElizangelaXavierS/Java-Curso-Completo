fb2e8fa0a9fbc0e33097917bbc90c594eead51fc
Elizangela Xavier <elizangelaxavier913@gmail.com>
2023-05-29 20:12:16 -0300

    Exercício 01

--- src/estrutura_sequencial/Exercicio01.java
+++ src/estrutura_sequencial/Exercicio01.java
@@ -1,7 +1,22 @@
 package estrutura_sequencial;
 
+import java.util.Scanner;
+
 /*
 
  */
 public class Exercicio01 {
+    public static void main(String[] args) {
+        Scanner leia = new Scanner(System.in);
+
+        System.out.println("Digite um número:");
+        int numero1 = leia.nextInt();
+
+        System.out.println("Digite o segundo número:");
+        int numero2 = leia.nextInt();
+
+        int soma = numero1 + numero2;
+
+        System.out.println("A Soma = " + soma);
+    }
 }
