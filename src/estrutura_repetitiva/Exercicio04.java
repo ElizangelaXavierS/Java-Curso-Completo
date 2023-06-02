c1e49724eb21da5f96d65217a451c24c81e063d7
Elizangela Xavier <elizangelaxavier913@gmail.com>
2023-06-02 17:20:49 -0300

    Exercício For

---
+++ src/estrutura_repetitiva/Exercicio04.java
@@ -0,0 +1,22 @@
+package estrutura_repetitiva;
+
+import java.util.Scanner;
+
+public class Exercicio04 {
+    public static void main(String[] args) {
+
+        Scanner leia = new Scanner(System.in);
+
+        System.out.println("Digite um número:");
+        int x = leia.nextInt();
+
+        int impares = 0;
+     for (int i = 1 ; i <= x; i++){
+         if( i % 2 != 0){
+             System.out.println(i);
+         }
+
+     }
+
+    }
+}
